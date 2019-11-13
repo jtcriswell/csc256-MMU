d73667aabf297e5b5a3a7cf6cdb1c5f048952d26
John Criswell <jtcriswel@gmail.com>
2019-11-13 09:47:08 -0500

    Added empty readMMU() and writeMMU() system calls.
    Renamed the source file for students to use to csc256.c.
    
    Added empty readMMU() and writeMMU() system calls.

--- linux-5.3.2/kernel/csc256.c
+++ linux-5.3.2/kernel/csc256.c
@@ -8,55 +8,12 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 
-struct prinfo {
-	/* current state of process */
-	long state;
+SYSCALL_DEFINE5(readMMU, unsigned long , vaddr, unsigned long * , pml4ep, unsigned long * , pdptep, unsigned long * , pdep, unsigned long * , ptep) {
+	printk ("CSC256: readMMU says hi!\n");
+	return 0;
+}
 
-	/* process id (input) */
-	pid_t pid;
-
-	/* process id of parent */
-	pid_t parent_pid;
-
-	/* process id of youngest child */
-	pid_t youngest_child_pid;
-
-	/* pid of the oldest among younger siblings */
-	pid_t younger_sibling_pid;
-
-	/* pid of the youngest among older siblings */
-	pid_t older_sibling_pid;
-
-	/* process start time */
-	unsigned long start_time;
-
-	/* CPU time spent in user mode */
-	unsigned long user_time;
-
-	/* CPU time spent in system mode */
-	unsigned long sys_time;
-
-	/* total user time of children */
-	unsigned long cutime;
-
-	/* total system time of children */
-	unsigned long cstime;
-
-	/* user id of process owner */
-	long uid;
-
-	/* name of program executed */
-	char comm[16];
-
-	/* The set of pending signals */
-	unsigned long signal;
-
-       	/* Number of open file descriptors */
-	unsigned long num_open_fds;
-};
-
-
-SYSCALL_DEFINE1(prinfo, struct prinfo *, p) {
-	printk ("CSC256: Hi!\n");
+SYSCALL_DEFINE5(writeMMU, unsigned long , vaddr, unsigned long , pml4e, unsigned long , pdpte, unsigned long , pde, unsigned long , pte) {
+	printk ("CSC256: writeMMU says hi!\n");
 	return 0;
 }
